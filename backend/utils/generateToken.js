1399e3fd962eeee11f9c3c59b50ea6007d745a98
Yug Patidar <135622619+yug1409@users.noreply.github.com>
2026-05-26 16:06:50 +0530

    Delete backend directory

--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const generateToken = (id, role) => {
-  return jwt.sign({ id, role }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
-};
-
-module.exports = generateToken;
